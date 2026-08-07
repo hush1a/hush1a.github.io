6f7ec856ae7440b5524370e12f3b1ce6f3c776d4
Bayu <bayuputraibana@mail.ugm.ac.id>
2026-08-07 18:48:17 +0700

    feat: include writeups in RSS feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -13,7 +13,10 @@ export async function GET(context: Context) {
   const projects = (await getCollection("projects"))
     .filter(project => !project.data.draft);
 
-  const items = [...blog, ...projects]
+  const writeups = (await getCollection("writeups"))
+    .filter(writeup => !writeup.data.draft);
+
+  const items = [...blog, ...projects, ...writeups]
     .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
 
   return rss({
